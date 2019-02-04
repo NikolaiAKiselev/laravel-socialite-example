4f3d7b6e9293e41c0cf6cef952520d3cecb9dc7c
Nikolai Kiselev <10960944+NikolaiAKiselev@users.noreply.github.com>
2019-02-04 20:18:21 +0400

    Fix comments

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -40,7 +40,7 @@ class LoginController extends Controller
     }
 
     /**
-     * Redirect the user to the GitHub authentication page.
+     * Redirect the user to the Google authentication page.
      *
      * @return \Illuminate\Http\Response
      */
@@ -50,7 +50,7 @@ class LoginController extends Controller
     }
 
     /**
-     * Obtain the user information from GitHub.
+     * Obtain the user information from Google.
      *
      * @return \Illuminate\Http\Response
      */
